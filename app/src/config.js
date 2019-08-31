37728876a8de9b6041fe032f8111781ccb1b6f82
minhdat97 <minhdat.tran97@gmail.com>
2019-08-31 18:08:17 +0700

    fix dockerfile

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,5 @@
 export const production = true; // set it to true when deploy to the server
 
 const domain = production ? '35.226.138.71:3001' : '127.0.0.1:3001'; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.comi
-export const websocketUrl = `wss://${domain}`
+export const websocketUrl = `ws://${domain}`
 export const apiUrl = `http://${domain}`
